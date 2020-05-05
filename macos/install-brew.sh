1bb317b7eeb8785b4ba2edf2eaac69b9610c8418
irotoris <shiroto00@yahoo.co.jp>
2020-05-05 16:19:05 +0900

    update brew script

--- macos/install-brew.sh
+++ macos/install-brew.sh
@@ -4,9 +4,7 @@ brew tap caskroom/cask
 brew tap caskroom/versions
 
 brew cask install iterm2
-
 brew cask install google-japanese-ime
-
 brew cask install google-chrome
 brew cask install firefox
 
@@ -14,14 +12,13 @@ brew cask install visual-studio-code
 brew cask install atom
 brew install git
 
-brew cask install java
+brew install zsh-completions
 
+brew cask install java
 brew install go
 brew install goenv
-
 brew install python
 brew install pyenv
-
 brew install rbenv
 brew install ruby-build
 
